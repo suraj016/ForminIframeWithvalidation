61c4d1955514aa2b03b9bda8a8856b5c51410807
Suraj Kumar <suraj.kr1450@gmail.com>
2023-05-09 20:50:54 +0530

    first commit

---
+++ js/countrystatecity.js
@@ -0,0 +1,217 @@
+
+function ajaxCall() {
+    this.send = function(data, url, method, success, type) {
+        type = 'json';
+        var successRes = function(data) {
+            success(data);
+        }
+
+        var errorRes = function(xhr, ajaxOptions, thrownError) {
+            
+            console.log(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+
+        }
+        jQuery.ajax({
+            url: url,
+            type: method,
+            data: data,
+            success: successRes,
+            error: errorRes,
+            dataType: type,
+            timeout: 60000
+        });
+
+    }
+
+}
+
+function locationInfo() {
+    var rootUrl = "https://geodata.phplift.net/api/index.php";
+    var call = new ajaxCall();
+
+
+
+    this.getCities = function(id) {
+        jQuery(".cities option:gt(0)").remove();
+        //get additional fields
+        
+        var url = rootUrl+'?type=getCities&countryId='+ '&stateId=' + id;
+        var method = "post";
+        var data = {};
+        jQuery('.cities').find("option:eq(0)").html("Please wait..");
+        call.send(data, url, method, function(data) {
+            jQuery('.cities').find("option:eq(0)").html("Select City");
+                var listlen = Object.keys(data['result']).length;
+
+                if(listlen > 0)
+                {
+                    jQuery.each(data['result'], function(key, val) {
+
+                        var option = jQuery('<option />');
+                        option.attr('value', val.name).text(val.name);
+                        jQuery('.cities').append(option);
+                    });
+                }
+                
+
+                jQuery(".cities").prop("disabled",false);
+            
+        });
+    };
+
+    this.getStates = function(id) {
+        jQuery(".states option:gt(0)").remove();
+        jQuery(".cities option:gt(0)").remove();
+        //get additional fields
+        var stateClasses = jQuery('#stateId').attr('class');
+
+        
+        var url = rootUrl+'?type=getStates&countryId=' + id;
+        var method = "post";
+        var data = {};
+        jQuery('.states').find("option:eq(0)").html("Please wait..");
+        call.send(data, url, method, function(data) {
+            jQuery('.states').find("option:eq(0)").html("Select State");
+            
+                jQuery.each(data['result'], function(key, val) {
+                    var option = jQuery('<option />');
+                    option.attr('value', val.name).text(val.name);
+                    option.attr('stateid', val.id);
+                    jQuery('.states').append(option);
+                });
+                jQuery(".states").prop("disabled",false);
+            
+        });
+    };
+
+    this.getCountries = function() {
+        var url = rootUrl+'?type=getCountries';
+        var method = "post";
+        var data = {};
+        jQuery('.countries').find("option:eq(0)").html("Please wait..");
+        call.send(data, url, method, function(data) {
+            jQuery('.countries').find("option:eq(0)").html("Select Country");
+            
+            jQuery.each(data['result'], function(key, val) {
+                var option = jQuery('<option />');
+                
+                option.attr('value', val.name).text(val.name);
+                option.attr('countryid', val.id);
+                
+                jQuery('.countries').append(option);
+            });
+                // jQuery(".countries").prop("disabled",false);
+            
+        });
+    };
+
+}
+
+jQuery(function() {
+    var loc = new locationInfo();
+    loc.getCountries();
+    jQuery(".countries").on("change", function(ev) {
+        var countryId = jQuery("option:selected", this).attr('countryid');
+        if(countryId != ''){
+            loc.getStates(countryId);
+        }
+        else{
+            jQuery(".states option:gt(0)").remove();
+        }
+    });
+    jQuery(".states").on("change", function(ev) {
+        var stateId = jQuery("option:selected", this).attr('stateid');
+        if(stateId != ''){
+            loc.getCities(stateId);
+        }
+        else{
+            jQuery(".cities option:gt(0)").remove();
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+function clearErrors(){
+
+    errors = document.getElementsByClassName('formerror');
+    for(let item of errors)
+    {
+        item.innerHTML = "";
+    }
+    
+    
+    }
+    function seterror(id, error){
+    //sets error inside tag of id 
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
+    
+    }
+    
+    function validateForm(){
+    var returnval = true;
+    clearErrors();
+    
+    //perform validation and if validation fails, set the value of returnval to false
+    var name = document.forms['myForm']["fname"].value;
+    if (name.length<5){
+        seterror("name", "*Length of name is too short");
+        returnval = false;
+    }
+    
+    if (name.length == 0){
+        seterror("name", "*Length of name cannot be zero!");
+        returnval = false;
+    }
+    
+    var email = document.forms['myForm']["femail"].value;
+    if (email.length>15){
+        seterror("email", "*Email length is too long");
+        returnval = false;
+    }
+    
+    var phone = document.forms['myForm']["fphone"].value;
+    if (phone.length != 10){
+        seterror("phone", "*Phone number should be of 10 digits!");
+        returnval = false;
+    }
+    
+    var state = document.forms['myForm']["fstate"].value;
+    if (name.length == 0){
+        seterror("name", "*Length of name cannot be zero!");
+        returnval = false;
+    }
+    
+    var country = document.forms['myForm']["fcountry"].value;
+    if (name.length == 0){
+        seterror("name", "*Length of name cannot be zero!");
+        returnval = false;
+    }
+    
+    var password = document.forms['myForm']["fpass"].value;
+    if (password.length < 6){
+    
+        // Quiz: create a logic to allow only those passwords which contain atleast one letter, one number and one special character and one uppercase letter
+        seterror("pass", "*Password should be atleast 6 characters long!");
+        returnval = false;
+    }
+    
+    var cpassword = document.forms['myForm']["fcpass"].value;
+    if (cpassword != password){
+        seterror("cpass", "*Password and Confirm password should match!");
+        returnval = false;
+    }
+    
+    return returnval;
+    }
+
